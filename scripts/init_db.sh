566cdbca3698d48d7e6f5d54feef3c4a503138c3
almcr <alm.dev2@gmail.com>
2022-03-20 23:52:21 +0100

    update script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,7 +3,7 @@ set -x
 set -e pipefail
 
 # Check if a custom db user name have been set
-MONGO_USERNAME="${MONGO_USER:=db_user}"
+MONGO_USERNAME="${MONGO_USERNAME:=db_user}"
 # Check if a custom password has been set, otherwise default to 'password'
 MONGO_PASSWORD="${MONGO_PASSWORD:=password}"
 # Check if a custom port has been set
