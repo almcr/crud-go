5c9260696112ecbb5e1d031ba7fe06003872befe
almcr <alm.dev2@gmail.com>
2022-03-21 01:29:12 +0100

    update main

--- main.go
+++ main.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"log"
+
+	"github.com/almcr/crud-go/app"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	server := app.NewServer()
+	server.Run()
+}
